213d2a39a4ac862197bcb8305c52d0874a7cbe19
SanikaC1999 <131801208+SanikaC1999@users.noreply.github.com>
2024-03-03 00:52:59 +0530

    error2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,43 +1,50 @@
 import React, { useEffect, useState } from "react";
 import styles from './Flag.module.css'
 
-export default function Flag (){
-    const [countries,setCountries] = useState([]);
+export default function Flag() {
+    const [countries, setCountries] = useState([]);
+    const [filteredCountries, setFilteredCountries] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch("https://restcountries.com/v3.1/all")
-        .then((res)=>res.json())
-        .then((data)=>setCountries(data))
-        .catch((err)=>console.error("error from data:",err))
-    },[])
+            .then((res) => res.json())
+            .then((data) => {
+                setCountries(data);
+                setFilteredCountries(data);
+            })
+            .catch((err) => console.error("error from data:", err))
+    }, [])
 
     const handleSearch = (event) => {
-      const searchValue = event.target.value.toLowerCase();
-      const filteredCountries = countries.filter((country) =>
-        country.name.common.toLowerCase().includes(searchValue)
-      );
-      setCountries(filteredCountries);
+        const searchValue = event.target.value.toLowerCase();
+        const filteredCountries = countries.filter((country) =>
+            country.name.common.toLowerCase().includes(searchValue)
+        );
+        setFilteredCountries(filteredCountries);
     };
-    
 
     return (
-      <>
-        <input
-        type="text"
-        placeholder="Search for a country"
-        onChange={handleSearch}
-      />
-        <div className={styles.main}>
-    {countries.map((country) => (
-        <div key={country.cca3} className={styles.wrapper}>
-            <img 
-                src={country.flags.png}
-                alt={`Flag of ${country.name.common}`}
-                className={styles.imgFlag} />
-            <h2 className={styles.heading}>{country.name.common}</h2>    
-        </div>
-    ))}
-        </div>
+        <>
+            <input
+                type="text"
+                placeholder="Search for a country"
+                onChange={handleSearch}
+            />
+            <div className={styles.main}>
+                {filteredCountries.map((country) => (
+                    <div key={country.cca3} className={styles.wrapper}>
+                        {country.flags && country.flags.png &&
+                            <img
+                                src={country.flags.png}
+                                alt={`Flag of ${country.name.common}`}
+                                className={styles.imgFlag} />
+                        }
+                        {country.name &&
+                            <h2 className={styles.heading}>{country.name.common}</h2>
+                        }
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
